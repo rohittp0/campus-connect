fdd6dc19ef4057058473a2126b992dd0863cc7c6
Rohit T P <tprohit9@gmail.com>
2023-06-20 21:41:19 +0530

    auto scoring done

--- public/js/add.js
+++ public/js/add.js
@@ -4,6 +4,10 @@ const college = document.querySelector('#college');
 const title = document.querySelector('#title');
 const description = document.querySelector('#description');
 
+const sentiment = new Promise((res) => {
+    const senti = ml5.sentiment('movieReviews', () => res(senti));
+});
+
 db.collection('colleges').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         const collegeOption = document.createElement('option');
@@ -17,9 +21,12 @@ db.collection('colleges').get().then((snapshot) => {
 document.getElementById("addForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    const {score} = (await sentiment).predict(description.value);
+
     await db.collection('colleges').doc(college.value).collection('reviews').add({
         title: title.value,
         description: description.value,
+        score
     });
 
     title.value = '';
